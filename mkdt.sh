b12c8020e89b705dd829f775ba8e36fd9dafbf8f
Say <sktjdgns1189@naver.com>
2015-07-15 23:57:34 +0900

    defconfig: build dtb

--- mkdt.sh
+++ mkdt.sh
@@ -1,4 +1,4 @@
 rm ./Image
-#rm ./dt.img
-#./dtbTool -o ./dt.img -s 2048 -p ./scripts/dtc/ ./arch/arm/boot/
+rm ./dt.img
+./dtbTool -o ./dt.img -s 2048 -p ./scripts/dtc/ ./arch/arm64/boot/dts/
 cp ./arch/arm64/boot/Image ./
